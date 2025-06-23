be747e34c113e6880333681ba981e13052ce7a2f
Joyce <140150177+joyce-yong@users.noreply.github.com>
2025-06-23 09:49:04 +0800

    Final

--- Assets/Scripts/UIStuff/OutroAutoLoadScene.cs
+++ Assets/Scripts/UIStuff/OutroAutoLoadScene.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class OutroAutoLoadScene : MonoBehaviour
 {
     public string sceneName = "MainMenu_VR";  // The scene you want to load
-    public float delay = 22f;                  // Delay in seconds
+    public float delay = 51f;                  // Delay in seconds
 
     void Start()
     {
